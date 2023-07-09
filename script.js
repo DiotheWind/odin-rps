094366a18571a5f755472fd14d59b687a2dbdccb
Deo Seth <diothewind@gmail.com>
2023-07-09 09:10:55 +0800

    Add playRound function which returns a string telling who won the game by evaluating the result of player and computer

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ function getComputerChoice() {
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    let message;
+
+    if (playerSelection === "rock" && computerSelection === "rock" ||
+    playerSelection === "paper" && computerSelection === "paper" ||
+    playerSelection === "scissors" && computerSelection === "scissors") {
+        message = `It's a tie! You both chose ${playerSelection}.`;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        message = "You lose! Paper beats Rock.";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        message = "You win! Rock beats Scissors.";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        message = "You win! Paper beats Rock.";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        message = "You lose! Scissors beats Paper.";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        message = "You lose! Rock beats Scissors.";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        message = "You win! Scissors beats Paper.";
+    }
+    return message;
+}
+
+console.log(playRound("scissors", "paper"));
+
